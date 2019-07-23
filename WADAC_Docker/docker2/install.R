f76537adea7a0b77e68495e5429bb21116241ead
Ragav Sridharan <ragav208@gmail.com>
2019-07-23 16:10:46 +0800

    Keras_reticulate fixed
    Get Keras and Reticulate from CRAN and added shiny server

--- WADAC_Docker/docker2/install.R
+++ WADAC_Docker/docker2/install.R
@@ -1,22 +1,24 @@
-install.packages('devtools')
-devtools::install_github('rstudio/keras')
-install.packages('reticulate')
-reticulate::use_python('/opt/conda/bin/python')
+#install.packages('devtools')
+#devtools::install_github('rstudio/keras')
+#install.packages('reticulate')
+#reticulate::use_python('/opt/conda/bin/python')
 
-require(tensorflow)
-require(reticulate)
-require(keras)
+#require(tensorflow)
+#require(reticulate)
+#require(keras)
 
-is_keras_available()
-system('which python')
-Sys.setenv(TENSORFLOW_PYTHON='/opt/conda/bin/python')
-use_python('/opt/conda/bin/python')
+#is_keras_available()
+#system('which python')
+#Sys.setenv(TENSORFLOW_PYTHON='/opt/conda/bin/python')
+#use_python('/opt/conda/bin/python')
 
-py_discover_config('tensorflow')
-py_discover_config('keras')
-is_keras_available()
+#py_discover_config('tensorflow')
+#py_discover_config('keras')
+#is_keras_available()
 
-packages = c("ggplot2",
+packages = c("keras",
+	     "reticulate",
+	     "ggplot2",
              "dplyr",
              "RSQLite",
              "magrittr",
@@ -35,6 +37,18 @@ packages = c("ggplot2",
 for (package in packages) {
     install.packages(package)
 }
+
+library(reticulate)
+library(keras)
+reticulate::use_python('/opt/conda/bin/python')
+
+system('which python')
+Sys.setenv(TENSORFLOW_PYTHON='/opt/conda/bin/python')
+use_python('/opt/conda/bin/python')
+
+py_discover_config('tensorflow')
+py_discover_config('keras')
+is_keras_available()
 
 
 library(RSQLite)
